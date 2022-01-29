50210fd322446694bcfa15667c48e7416db28126
Jacob-Irvin <70180707+Jacob-Irvin@users.noreply.github.com>
2022-01-29 11:56:05 -0700

    Add files via upload

---
+++ HW 2/style.js
@@ -0,0 +1,3 @@
+console.log(
+    "My favorite websites are probably Nordstrom, Levi's, and Urban Outfitters because I love fashion. My favorite games are the Borderlands games, the Elder Scrolls games, and the Halo games. My favorite artsts at the moment are Anderson Paak, Tyler the Creator, and Kid Kudi"
+); 
